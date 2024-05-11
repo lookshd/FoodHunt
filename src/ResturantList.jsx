c75768fea3a1e2ae1455b5a32c906d1c95df9895
ABHISHEK MISHRA <89726834+lookshd@users.noreply.github.com>
2024-05-11 21:10:07 +0530

    Update ResturantList.jsx

--- src/ResturantList.jsx
+++ src/ResturantList.jsx
@@ -1,12 +1,12 @@
 export const IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
   export const CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
 
 export const GET_RESTAURANTS_URL = process.env.REACT_APP_SWIGGY_API;
 // Image CDN URL for Restaurant Menu
 export const ITEM_IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+ "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
 // Swiggy API to get Restaurant data with foodfire erver
 export const swiggy_api_URL =
